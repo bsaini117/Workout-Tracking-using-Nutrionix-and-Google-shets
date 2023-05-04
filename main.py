71f8b507dbe53cbee3daa6ba1cc94c6ccb57330f
bsaini117 <70615656+bsaini117@users.noreply.github.com>
2023-05-04 14:50:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ today_formatted = today.strftime("%x")
 time_formatted = today.strftime("%X")
 
 
-API_KEY = "ba46ca79364f7d18eacda0208d27c404"
-API_ID = "e8a09bd0"
+API_KEY = "INSERT API"
+API_ID = "INSERT KEY"
 
 sheety_endpoint = "https://api.sheety.co/a5b9f38df3cc01445d54b5e990cb5e1d/workoutTracking/workouts"
-sheety_token = "k1!6iH4pmOto"
+sheety_token = "INSERT API"
 
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
